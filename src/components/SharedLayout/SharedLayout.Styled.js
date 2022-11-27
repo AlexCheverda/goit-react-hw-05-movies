c2a854d027bc550612a89e68e22ca21d8702b11c
Aleksandr Cheverda <alexar@ua.fm>
2022-11-27 21:43:50 +0200

    Update SharedLayout.Styled.js

--- src/components/SharedLayout/SharedLayout.Styled.js
+++ src/components/SharedLayout/SharedLayout.Styled.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  padding: 0px 20px;
+  padding: 0px 30px;
   margin: 0 auto;
   min-height: 100vh;
   background: linear-gradient(
@@ -11,24 +11,24 @@ export const Container = styled.div`
     #bebf00 0%,
     #ebe212 29%,
     #337eef 56%,
-    #05a3ff 74%,
-    #090093 100%
+    #090093 74%,
+    #05a3ff 100%
   );
 `;
 export const Header = styled.header`
-  padding: 20px;
-  border-radius: 4px;
-  box-shadow: 1px 2px 13px 3px rgba(0, 0, 0, 0.78);
+  padding: 30px;
+  border-radius: 5px;
+  box-shadow: 2px 3px 15px 5px rgba(0, 0, 0, 0.74);
 `;
 
 export const Nav = styled.nav`
   display: flex;
-  column-gap: 20px;
+  column-gap: 30px;
 `;
 
 export const Link = styled(NavLink)`
   padding: 8px 26px;
-  border-radius: 4px;
+  border-radius: 15px;
   text-decoration: none;
   color: black;
   font-weight: 500;
